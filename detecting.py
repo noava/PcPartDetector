bf584309d1d93cbf4090a523752a286d0c11894b
Niklas Vezina <niklasv@uia.no>
2023-11-08 02:46:18 +0100

    moved a few lines in detecting py

--- detecting.py
+++ detecting.py
@@ -4,18 +4,20 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = YOLO("C:/Users/nikla/ikt213g23h/assignments/solutions/PCPartDetector/runs/detect/train8/weights/best.pt").to(device)
+    model = YOLO("./runs/detect/train8/weights/best.pt").to(device)
 
     # Detect the image
 
     # Load the image
-    image = cv2.imread("C:/Users/nikla/ikt213g23h/assignments/solutions/PCPartDetector/TestImg/ram.jpg")
+    image = cv2.imread("./TestImg/ram.jpg")
+
+    # Image:
+    results = model.predict(image, show=True, save=True)
 
     # Camera:
     results = model.predict(source="1", show=True)
 
-    # Image:
-    # results = model.predict(image, show=True, save=True, save_dir="C:/Users/nikla/ikt213g23h/assignments/solutions/PCPartDetector/TestImg/")
+
 
     # Wait for keyboard press
     cv2.waitKey(0)
